421eca50515ed28957b6cef6efc26f9c7b92bbde
dmm <505991554@qq.com>
2020-07-16 19:26:03 +0100

    day4 算法训练营

--- Week_01/day4/24两两交换链表中的节点.py
+++ Week_01/day4/24两两交换链表中的节点.py
@@ -14,11 +14,25 @@
 
 # leetcode submit region begin(Prohibit modification and deletion)
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
-class Solution:
-    def swapPairs(self, head: ListNode) -> ListNode:
+
+def swapPairs(head) :
 # leetcode submit region end(Prohibit modification and deletion)
+    thead=ListNode(-1)
+    thead.next=head
+    c=thead
+    while c.next and c.next.next :
+        a,b = c.next , c.next.next
+        c.next , a.next = b , b.next
+        b.next = a
+        c = c.next.next
+    return thead.next
+
+
+
+
+
