ef917aa642c66370ab9e57ddf0790a5d73401137
dmm <505991554@qq.com>
2020-08-26 22:25:37 +0100

    训练营第六周

---
+++ Week_06/242 有效的字母异位词.py
@@ -0,0 +1,38 @@
+# 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+#
+#  示例 1:
+#
+#  输入: s = "anagram", t = "nagaram"
+# 输出: true
+#
+#
+#  示例 2:
+#
+#  输入: s = "rat", t = "car"
+# 输出: false
+#
+#  说明:
+# 你可以假设字符串只包含小写字母。
+#
+#  进阶:
+# 如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？
+#  Related Topics 排序 哈希表
+#  👍 237 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        se = set(s)
+        st = set(t)
+        if se == st:
+            for i in se:
+                if s.count(i) != t.count(i):
+                    return False
+            return True
+
+        return False
+
+# leetcode submit region end(Prohibit modification and deletion)
--- Week_06/README.md
+++ Week_06/README.md
@@ -1,1 +1,4 @@
 学习笔记
+
+
+[优秀trick](https://leetcode-cn.com/problems/reverse-words-in-a-string-iii/solution/python-fan-zhuan-zi-fu-chuan-zhong-dan-ci-si-lu-xi/)
